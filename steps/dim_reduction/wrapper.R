42af8998a271b9311fb5fd0a810495911e30019a
Fabio Pohl <fabio.baldivia.pohl@ki.se>
2019-12-06 13:03:03 +0100

    Add wrapper for dimension reduction

--- steps/dim_reduction/wrapper.R
+++ steps/dim_reduction/wrapper.R
@@ -1,10 +1,12 @@
 #' Reduce dimensionality using Seurat
 #' 
 #' @param input Seurat R object
+#' @return Seurat object with PCA and UMAP slots
 suppressMessages(library(Seurat))
 
 # Get input
-seurat_file <- snakemake@input
+set.seed(666)
+seurat_file <- snakemake@input[[1]]
 
 # Load Seurat object
 seurat <- readRDS(seurat_file)
@@ -18,8 +20,13 @@ seurat <- ScoreJackStraw(seurat, dim = 1:20)
 sig_pcs <- sum(seurat@reductions$pca@jackstraw$overall.p.values < 0.01)
 
 # Run UMAP
-seurat <- RunUMAP(seurat, dim = 1:sig_pcs)
+seurat <- RunUMAP(seurat, n.components = 3, dim = 1:sig_pcs)
+
+# Get output
+output_file <- snakemake@output[[1]]
+
+# Save seurat object
+saveRDS(file = output_file, seurat)
 
 # Testing
-seurat_file <- "/data/proj/GCB_FBP/repos/seurat_pipeline/steps/dim_reduction/test/seurat_obj.rds"
-
+#seurat_file <- "/data/proj/GCB_FBP/repos/seurat_pipeline/steps/dim_reduction/test/seurat_obj.rds"
