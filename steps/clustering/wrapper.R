11da4a956d1c79a4d844f13c984735b68b5106a1
Fabio Pohl <fabio.baldivia.pohl@ki.se>
2019-12-06 13:21:07 +0100

    Add clustering wrapper

---
+++ steps/clustering/wrapper.R
@@ -0,0 +1,25 @@
+#' Clustering using Seurat
+#'
+#' @param input Seurat Object with reduced dimensions
+#' @return Seurat Object with clusters information
+suppressMessages(library(Seurat))
+
+# Get input
+seurat_file <- snakemake@input[[1]]
+
+# Load Seurat object
+seurat <- readRDS(seurat_file)
+ndims <- length(seurat@commands$RunUMAP.SCT.pca$dims)
+
+# Cluster cells
+seurat <- FindNeighbors(seurat, dim = 1:ndims)
+seurat <- FindClusters(seurat)
+
+# Get output
+output <- snakemake@output[[1]]
+
+# Save Seurat Object
+saveRDS(file = output, seurat)
+
+# Testing
+#seurat_file <- "/data/proj/GCB_FBP/repos/seurat_pipeline/steps/clustering/test/seurat_obj_red.rds"
