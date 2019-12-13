5a70ee56b02a7c3c3ef9a4d73d83f8bf2257c4af
Fabio Pohl <fabio.baldivia.pohl@ki.se>
2019-12-13 17:35:09 +0100

    Add wrapper to 2D and 3D UMAP

---
+++ steps/visualization/umap/wrapper.R
@@ -0,0 +1,31 @@
+#' UMAP for clustering
+#' 
+#' @param seurat Seurat object after UMAP and clustering
+suppressMessages(library(Seurat))
+suppressMessages(library(plotly))
+
+# Get input
+seurat_file <- snakemake@input[["seurat"]]
+
+# Get output
+umap_file <- snakemake@output[["umap"]]
+html_file <- snakemake@output[["html"]]
+
+# Load seurat object
+seurat <- readRDS(seurat_file)
+
+# Plot 2D UMAP
+setEPS()
+postscript(umap_file)
+DimPlot(seurat, reduction = "umap")
+temp <- dev.off()
+
+# Plot 3D UMAP
+umap_emb <- Embeddings(seurat, reduction = "umap")[, 1:3]
+p <- plot_ly(type = "scatter3d", x = umap_emb[, 1], y = umap_emb[, 2], z = umap_emb[, 3], mode = "markers", color = seurat$seurat_clusters, text = seurat$seurat_clusters, size = 1)
+
+htmlwidgets::saveWidget(as_widget(p), html_file)
+
+# Testing
+#seurat_file <- "/data/proj/GCB_FBP/repos/seurat_pipeline/steps/visualization/umap/test/seurat_obj_red_cluster.rds"
+
