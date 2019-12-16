1009e6e2109a7690425434292790b17677bdc4c9
Fabio Pohl <fabio.baldivia.pohl@ki.se>
2019-12-16 11:55:52 +0100

    Conclude expression plot wrapper

--- steps/visualization/expression/wrapper.R
+++ steps/visualization/expression/wrapper.R
@@ -1,27 +1,53 @@
 #' Plot expression of features of interest in UMAP
 #'
 #' @param seurat Seurat object after UMAP
+#' @return Feature plots 
 suppressMessages(library(Seurat))
+suppressMessages(library(plotly))
+suppressMessages(library(RColorBrewer))
 
 # Get input
 seurat_file <- snakemake@input[["seurat"]]
-features_file <- snakemake@input[["features"]]
+
+# Get feature
+feat <- snakemake@params[["feat"]]
 
 # Get output 
-umap <- snakemake@ouput[["umap"]]
+umap_file <- snakemake@output[["umap"]]
+html_file <- snakemake@output[["html"]]
 
 # Load Seurat Object
 seurat <- readRDS(seurat_file)
-
-# Load features to plot
-feats <- read.table(features_file, stringsAsFeatures = FALSE)
+DefaultAssay(seurat) <- "RNA"
 
 # Plot features
 setEPS()
-postscript("~/feats.eps")
-FeaturePlot(seurat, features = feats)
+postscript(umap_file)
+FeaturePlot(seurat, features = feat)
 temp <- dev.off()
 
+# Plot 3D UMAP
+cols <- brewer.pal(9, "OrRd")
+pal <- colorRampPalette(cols)
+
+metadata <- colnames(seurat[[]])
+umap_emb <- Embeddings(seurat, reduction = "umap")[, 1:3]
+
+if (feat %in% metadata) {
+    color_feat <- seurat[[feat]][[1]]
+    p <- plot_ly(type = "scatter3d", x = umap_emb[, 1], y = umap_emb[, 2], z = umap_emb[, 3], mode = "markers", color = color_feat, colors = pal(20), size = 1)
+    htmlwidgets::saveWidget(as_widget(p), html_file)
+} else {
+    color_counts <- as.vector(seurat[["RNA"]][feat, ])
+    p <- plot_ly(type = "scatter3d", x = umap_emb[, 1], y = umap_emb[, 2], z = umap_emb[, 3], mode = "markers", color = color_counts, colors = pal(20), size = 1)
+    htmlwidgets::saveWidget(as_widget(p), html_file)
+}
+
 # Testing
-seurat_file <- "/data/proj/GCB_FBP/repos/seurat_pipeline/steps/visualization/expression/test/seurat_obj_red_cluster.rds"
-feats <- c("nCount_RNA", "nFeature_RNA", "percent.mt", "PTPRZ1", "MBP")
+#seurat_file <- "/data/proj/GCB_FBP/repos/seurat_pipeline/steps/visualization/expression/test/seurat_obj_red_cluster.rds"
+#umap_file <-  "/data/proj/GCB_FBP/repos/seurat_pipeline/steps/visualization/expression/test/umap_ncount.eps"
+#html_file <-  "/data/proj/GCB_FBP/repos/seurat_pipeline/steps/visualization/expression/test/3dumap_ncount.html"
+#feats <- c("nCount_RNA", "nFeature_RNA", "percent.mt", "PTPRZ1", "MBP")
+#features_file <- "/data/proj/GCB_FBP/repos/seurat_pipeline/steps/visualization/expression/test/features_example.txt"
+#feat <- "nCount_RNA"
+#feat <- "PTPRZ1"
