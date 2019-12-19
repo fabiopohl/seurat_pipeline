bf600439bcadf982396bc3bee24dfd8e537ec003
Fabio Pohl <fabio.baldivia.pohl@ki.se>
2019-12-19 10:46:54 +0100

    Changes in feature plot to deal with gene not found

--- steps/visualization/expression/wrapper.R
+++ steps/visualization/expression/wrapper.R
@@ -19,30 +19,48 @@ html_file <- snakemake@output[["html"]]
 # Load Seurat Object
 seurat <- readRDS(seurat_file)
 DefaultAssay(seurat) <- "RNA"
-
-# Plot features
-setEPS()
-postscript(umap_file)
-FeaturePlot(seurat, features = feat)
-temp <- dev.off()
-
-# Plot 3D UMAP
-cols <- brewer.pal(9, "OrRd")
-pal <- colorRampPalette(cols)
-
 metadata <- colnames(seurat[[]])
-umap_emb <- Embeddings(seurat, reduction = "umap")[, 1:3]
 wd <- getwd()
 html_output <- paste0(wd, "/", html_file)
+#html_output <- html_file # testing
 
-if (feat %in% metadata) {
-    color_feat <- seurat[[feat]][[1]]
-    p <- plot_ly(type = "scatter3d", x = umap_emb[, 1], y = umap_emb[, 2], z = umap_emb[, 3], mode = "markers", color = color_feat, colors = pal(20), size = 1)
-    htmlwidgets::saveWidget(as_widget(p), html_output)
+if (feat %in% c(rownames(seurat), metadata)){
+        
+    # Plot features
+    setEPS()
+    postscript(umap_file)
+    p <- FeaturePlot(seurat, features = feat)
+    print(p)
+    temp <- dev.off()
+    # Plot 3D UMAP
+    cols <- brewer.pal(9, "OrRd")
+    pal <- colorRampPalette(cols)
+    umap_emb <- Embeddings(seurat, reduction = "umap")[, 1:3]
+    
+    if (feat %in% metadata) {
+        color_feat <- seurat[[feat]][[1]]
+        p <- plot_ly(type = "scatter3d", x = umap_emb[, 1], y = umap_emb[, 2], z = umap_emb[, 3], mode = "markers", color = color_feat, colors = pal(20), size = 1)
+        htmlwidgets::saveWidget(as_widget(p), html_output)
+    } else {
+        color_counts <- as.vector(seurat[["RNA"]][feat, ])
+        p <- plot_ly(type = "scatter3d", x = umap_emb[, 1], y = umap_emb[, 2], z = umap_emb[, 3], mode = "markers", color = color_counts, colors = pal(20), size = 1)
+        htmlwidgets::saveWidget(as_widget(p), html_output)
+    }
+
 } else {
-    color_counts <- as.vector(seurat[["RNA"]][feat, ])
-    p <- plot_ly(type = "scatter3d", x = umap_emb[, 1], y = umap_emb[, 2], z = umap_emb[, 3], mode = "markers", color = color_counts, colors = pal(20), size = 1)
-    htmlwidgets::saveWidget(as_widget(p), html_output)
+    setEPS()
+    postscript(umap_file)
+    par(mar = c(0,0,0,0))
+    plot(c(0, 1), c(0, 1), ann = F, bty = 'n', type = 'n', xaxt = 'n', yaxt = 'n') 
+    text(x = 0.5, y = 0.5, paste0("Feature not found: ", feat), cex = 1.6, col = "black")
+    temp <- dev.off()
+
+    setEPS()
+    postscript(html_file)
+    par(mar = c(0,0,0,0))
+    plot(c(0, 1), c(0, 1), ann = F, bty = 'n', type = 'n', xaxt = 'n', yaxt = 'n') 
+    text(x = 0.5, y = 0.5, paste0("Feature not found: ", feat), cex = 1.6, col = "black")
+    temp <- dev.off()
 }
 
 # Testing
@@ -52,4 +70,7 @@ if (feat %in% metadata) {
 #feats <- c("nCount_RNA", "nFeature_RNA", "percent.mt", "PTPRZ1", "MBP")
 #features_file <- "/data/proj/GCB_FBP/repos/seurat_pipeline/steps/visualization/expression/test/features_example.txt"
 #feat <- "nCount_RNA"
-#feat <- "PTPRZ1"
+#feat <- "NONAAA"
+#seurat_file <- "/Users/fabiopohl/proj/czi_kb_lidx_191108/results/11723WAPool01-S__18_13782/seurat_obj_red_cluster_1700.rds"
+#html_file <- "~/test1.html"
+#umap_file <- "~/test2.eps"
