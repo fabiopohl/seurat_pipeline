e54b096ccaf339f43715b5a7336f79f4589cab64
Fabio Pohl <fabio.baldivia.pohl@ki.se>
2020-01-08 16:34:03 +0100

    Changes in integration wrapper

--- steps/integration/wrapper.R
+++ steps/integration/wrapper.R
@@ -3,9 +3,8 @@
 #' @param seurat List of seurat objects
 #' @param samples List of samples to integrate
 #' @return Single seurat object with integrated data
-suppressMessages(library(Seurat))
+suppressMessages(library(Seurat, lib="/home/fabio/R/x86_64-redhat-linux-gnu-library/3.6"))
 options(future.globals.maxSize =  10485760000)
-#limit <- 10000*1024^2
 
 # Get input
 seurat_files <- snakemake@input[["seurat"]]
@@ -18,13 +17,19 @@ seurat_list <- lapply(seurat_files, readRDS)
 seurat_list <- lapply(1:length(seurat_list), function(idx) {
                           RenameCells(seurat_list[[idx]], new.names=paste0(samples[idx], "_", colnames(seurat_list[[idx]])))
 })
-seurat1 <- CreateSeuratObject(counts = GetAssayData(object = seurat_list[[1]], slot="counts"))
-seurat2 <- CreateSeuratObject(counts = GetAssayData(object = seurat_list[[2]], slot="counts"))
 
-seurat_list <- list(seurat1, seurat2)
-for (i in 1:length(seurat_list)) {
-    seurat_list[[i]] <- SCTransform(seurat_list[[i]], verbose = FALSE)
+# Redoing 
+# Get counts data
+new_seurat <- lapply(1:length(seurat_list), function(i) {
+                         CreateSeuratObject(counts = (GetAssayData(object = seurat_list[[i]], slot = "counts")))
+})
+
+# Normalise
+for (i in 1:length(new_seurat)) {
+    new_seurat[[i]] <- SCTransform(new_seurat[[i]], verbose = FALSE)
 }
+seurat_list <- new_seurat
+
 # Integrate datasets
 seurat_features <- SelectIntegrationFeatures(object.list = seurat_list, nfeatures = 3000)
 seurat_list <- PrepSCTIntegration(object.list = seurat_list, anchor.features = seurat_features)
@@ -39,6 +44,6 @@ output_file <- snakemake@output[[1]]
 saveRDS(file = output_file, seurat_integrated)
 
 # Testing
-seurat_files <- c("test/seurat_obj_red_cluster_1.rds",  "test/seurat_obj_red_cluster_2.rds")
-samples <- c("sample1", "sample2")
-output_file <- "test/seurat_obj_integrated.rds"
+#seurat_files <- c("test/seurat_obj_red_cluster_1.rds",  "test/seurat_obj_red_cluster_2.rds")
+#samples <- c("sample1", "sample2")
+#output_file <- "test/seurat_obj_integrated.rds"
